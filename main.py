b75c5b77e58a3e6fed5fbc0d8408fd90057af422
Rusty Mann <mannrusty@gmail.com>
2018-08-04 13:05:21 -0700

    mostly working now, still working on heroku part.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+#app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 def get_fact():
@@ -19,7 +20,14 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+
+    fact = get_fact()
+    pig_url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+
+    response = requests.post(pig_url, data={'input_text': fact}, allow_redirects=False,)
+    url_header = response.headers['Location']
+
+    return <a href={}>{}</a>.format(url_header, url_header)
 
 
 if __name__ == "__main__":
